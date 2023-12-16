06622c1d578a41cc45a01794a9bc1e34d46eaa1f
mruqqie <mruqqie@gmail.com>
2023-12-16 11:57:09 +0100

    modified:   app/app/layout.tsx 	modified:   app/app/loginWindow.tsx 	modified:   app/storage/auth.json

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -57,7 +57,7 @@ export default async function RootLayout({
     /*
       TODO #1: Indicate that this function is a server function by adding 'use server';
     */
-
+    'use server';
     /*
       TODO #2: Create the new User object with a username, name, and privateKey
     
@@ -65,6 +65,7 @@ export default async function RootLayout({
         - 
         - Use the newPrivateKey() function to generate a new private key for the user
     */
+    const privateKey = newPrivateKey();
 
     /* 
       TODO #3: Store the user in the local account cache
@@ -83,7 +84,29 @@ export default async function RootLayout({
           from the local account cache. Then, throw the error to be caught by the catch block in
           the loginWindow.tsx file.
     */
-  }
+    const username = form.get('username');
+    const name = form.get('name');
+
+    if (username === null || name === null) {
+      console.error('Username or name is null.');
+    } else {
+      const newUser = {
+        username: username.toString(),
+        name: name.toString(),
+        privateKey: privateKey,
+      };
+
+      try {
+        await storeUser(newUser); // TODO #3
+        console.log('User stored successfully');
+        await createProfile(newUser);
+        await login(newUser);
+      } catch (error: any) {
+        await dropUser(newUser);
+        throw error;
+      }
+    }
+  };
 
   if (!me) {
     return (
@@ -94,13 +117,13 @@ export default async function RootLayout({
           <div className='absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center'>
             <div className='w-full max-w-xs space-y-6 rounded-xl border border-neutral-300 bg-neutral-400 px-6 py-4'>
               <p className='text-2xl font-bold'>Log in to Over Network</p>
-              {
-                await getNumberOfUsers() >= 2 ? 
+              {(await getNumberOfUsers()) >= 2 ? (
                 <p className='text-sm font-medium text-neutral-100'>
                   You have reached the maximum number of accounts.
-                </p> :
-                <LoginWindow setUpProfile={setUpProfile} /> 
-              }
+                </p>
+              ) : (
+                <LoginWindow setUpProfile={setUpProfile} />
+              )}
               <Accounts />
             </div>
           </div>
@@ -113,19 +136,18 @@ export default async function RootLayout({
     <html lang='en'>
       <body className={`${cal.variable} ${matter.variable} ${inter.className}`}>
         <div className='flex h-screen flex-row items-center justify-center'>
-          <form action={logout} className='flex flex-col gap-3 border rounded-xl px-4 py-2 bg-neutral-400 border-neutral-300'>
-            <p className='text-2xl font-bold'>
-              You are logged in!
-            </p>
+          <form
+            action={logout}
+            className='flex flex-col gap-3 rounded-xl border border-neutral-300 bg-neutral-400 px-4 py-2'
+          >
+            <p className='text-2xl font-bold'>You are logged in!</p>
             <p className='text-xs font-bold uppercase text-neutral-100'>
               Your account
             </p>
             <div className='flex flex-row items-center justify-start space-x-3 rounded-xl border border-neutral-200 bg-neutral-300 px-4 py-2'>
               <Avatar className='h-11 w-11' user={me} />
               <div className='flex flex-col items-start -space-y-0.5'>
-                <p className='text-lg font-medium capitalize'>
-                  {me.name}
-                </p>
+                <p className='text-lg font-medium capitalize'>{me.name}</p>
                 <p className='text-neutral-100'>@{me.username}</p>
               </div>
             </div>
